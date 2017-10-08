2b827ddf0649afd77b419bbb801fc897ea0a51eb
Volkadav001 <andrewhoma@mail.ru>
2017-10-08 10:22:36 +0300

    Супер лвл-дизайн Ондрея

---
+++ Snow2/Assets/Bot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : MonoBehaviour {
+    public GameObject point_0;
+    public GameObject point_1;
+    public GameObject point_2;
+
+    Transform[] all_Point = new Transform[3];
+
+    private Vector3 target_Pos;
+
+    private int i = 0;
+    public float speed_move = 5f;
+
+    // Use this for initialization
+    void Start () {
+
+        point_0 = GameObject.FindGameObjectWithTag("Point1");
+        point_1 = GameObject.FindGameObjectWithTag("Point2");
+        point_2 = GameObject.FindGameObjectWithTag("Point3");
+
+        all_Point[0] = point_0.transform;
+        all_Point[1] = point_1.transform;
+        all_Point[2] = point_2.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // if (SnowboardScript.script_flag)
+        //{
+            target_Pos = all_Point[1].transform.position;
+            transform.Translate(Vector3.Normalize(target_Pos - transform.position) * Time.deltaTime * speed_move);
+            float distance = Vector3.Distance(target_Pos, transform.position);
+            if (distance < 0.5f)
+            {
+                if (i < all_Point.Length - 1)
+                {
+                    i++;
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+            }
+
+        //}
+    }
+}
--- Snow2/Assets/SnowboardScript.cs
+++ Snow2/Assets/SnowboardScript.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class SnowboardScript : MonoBehaviour
 {
-
-
+    //public static bool script_flag = false;
+    //public GameObject Sphere;
     public float speed = 100;
     public float speedYRotation = 100;
     public float speedXRotation = 200;
@@ -15,7 +15,7 @@ public class SnowboardScript : MonoBehaviour
     public bool flopOrRespawn = true;
     public Vector3 respawnPos = Vector3.zero;
     public Vector3 respawnRot = Vector3.zero;
-    bool isFly=false;
+    bool isFly = false;
 
     // Use this for initialization
 
@@ -26,17 +26,17 @@ public class SnowboardScript : MonoBehaviour
         this.transform.Translate(new Vector3(0, 1f, 0));
         float t = this.transform.rotation.eulerAngles.y;
         this.transform.rotation = new Quaternion(0, 0, 0, 0);
-        this.transform.Rotate(0,t,0);
+        this.transform.Rotate(0, t, 0);
         this.transform.Translate(new Vector3(0, 1, 0));
         Debug.Log("Flop");
-        
+
     }
 
     void respawn()
     {
         this.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
         this.transform.rotation = new Quaternion(0, 0, 0, 0);
-        this.transform.Rotate(respawnRot); 
+        this.transform.Rotate(respawnRot);
         this.transform.position = respawnPos;
     }
 
@@ -44,21 +44,22 @@ public class SnowboardScript : MonoBehaviour
     {
     }
 
-    void OnTriggerEnter() {
+    void OnTriggerEnter()
+    {
 
         isFly = false;
     }
     void OnTriggerExit()
     {
         isFly = true;
-        
+
     }
     // Update is called once per frame
     void Update()
     {
 
 
-        
+
         if (upColider.GetComponent<upColliderScript>().onTrig)
         {
             if (flopOrRespawn)
@@ -72,24 +73,33 @@ public class SnowboardScript : MonoBehaviour
         if (!isFly)
         {
 
-            transform.Rotate(Input.GetAxis("Horizontal") *Vector3.up * speedYRotation * Time.deltaTime);
+            transform.Rotate(Input.GetAxis("Horizontal") * Vector3.up * speedYRotation * Time.deltaTime);
             transform.GetComponent<Rigidbody>().AddForce(transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime);
 
             if (Input.GetKeyDown(KeyCode.Space)) transform.GetComponent<Rigidbody>().AddForce(transform.up * jumpForce, ForceMode.Force);
-            
-           /* if (Input.GetKey(KeyCode.D)) transform.Rotate(Vector3.up * speedYRotation * Time.deltaTime);
-            if (Input.GetKey(KeyCode.A)) transform.Rotate(-Vector3.up * speedYRotation * Time.deltaTime);
-            if (transform.GetComponent<Rigidbody>().velocity.magnitude < maxSpeed) transform.GetComponent<Rigidbody>().AddForce(transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime);
-            */
+
+            /* if (Input.GetKey(KeyCode.D)) transform.Rotate(Vector3.up * speedYRotation * Time.deltaTime);
+             if (Input.GetKey(KeyCode.A)) transform.Rotate(-Vector3.up * speedYRotation * Time.deltaTime);
+             if (transform.GetComponent<Rigidbody>().velocity.magnitude < maxSpeed) transform.GetComponent<Rigidbody>().AddForce(transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime);
+             */
 
         }
-        else {
+        else
+        {
             if (Input.GetKey(KeyCode.W)) transform.Rotate(Vector3.right * speedXRotation * Time.deltaTime);
             if (Input.GetKey(KeyCode.S)) transform.Rotate(-Vector3.right * speedXRotation * Time.deltaTime);
             if (Input.GetKey(KeyCode.D)) transform.Rotate(Vector3.up * speedXRotation * Time.deltaTime);
             if (Input.GetKey(KeyCode.A)) transform.Rotate(-Vector3.up * speedXRotation * Time.deltaTime);
         }
-            
-            
+
+        //     }
     }
+
+   // void OnTriggerEnter(Collider other)
+    //{
+      //  if (other.gameObject.CompareTag("Trigger"))
+       // {
+       //     script_flag = true;
+       // }
+    //}
 }
